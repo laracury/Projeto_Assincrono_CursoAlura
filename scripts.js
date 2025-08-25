c993307173d88ef3d5c3ad5ee9dfb7660d1c7564
Lara Cury <laracury2@gmail.com>
2025-08-25 16:57:30 -0300

    Função de inserir imagem com prévia

--- scripts.js
+++ scripts.js
@@ -3,4 +3,38 @@ const inputUpload = document.getElementById('image-upload');
 
 uploadBtn.addEventListener('click', () => {
     inputUpload.click();
-});
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name})
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo);
+    }) 
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
+            const conteudoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoArquivo.url;
+            nomeDaImagem.textContent = conteudoArquivo.nome;
+        } catch (erro) {
+            console.error("Erro na Leitura do arquivo");
+
+        }
+    }
+})
